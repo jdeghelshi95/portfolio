b043da71a8be2df26587cbda1917d33e6380cea6
JOSHUA EGHELSHI <jdeghelshi1995@gmail.com>
2022-09-18 09:48:26 -0400

    made some changes to design

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,29 +4,29 @@ const Nav = () => {
   return (
 <div className=' h-fit'>
     <nav className='flex justify-center bg-gray-700 h-1/10'>
-            <ul className='flex justify-evenly w-full p-2'>
+            <ul className='flex justify-evenly w-full p-1'>
 
-                <div className='hover:bg-white w-2/12 bg-indigo-700'>
+                <div className='hover:bg-white w-2/12 bg-red-700'>
                 <li className='text-white  hover:text-black'><a href="/"> About</a></li>
                 </div>
 
-                <div className='hover:bg-white w-2/12  bg-indigo-600'>
+                <div className='hover:bg-white w-2/12  bg-red-600'>
                 <li className='text-white  hover:text-black'> <a href='skills'>Skills</a></li>
                 </div>
                 
-                <div className='hover:bg-white w-2/12 bg-indigo-700'>
+                <div className='hover:bg-white w-2/12 bg-red-700'>
                 <li className='text-white  hover:text-black'><a href='prof'>Profile</a> </li>
                 </div>
                 
-                <div className='hover:bg-white w-2/12 bg-indigo-600 '>
+                <div className='hover:bg-white w-2/12 bg-red-600 '>
                 <li className='text-white  hover:text-black '> <a href="signup">Signup</a></li>
                 </div>
 
-                <div className='hover:bg-white w-2/12 bg-indigo-700'>
+                <div className='hover:bg-white w-2/12 bg-red-700'>
                 <li className='text-white  hover:text-black'> <a href='login'>Signin</a> </li>
                 </div>
 
-                <div className='hover:bg-white w-2/12 bg-indigo-600'>
+                <div className='hover:bg-white w-2/12 bg-red-600'>
                 <li className='text-white  hover:text-black'><a href='logout'>logout</a></li>
                 </div>
             </ul>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div className="h-screen">
+    <div className="h-screen ">
       <div className="w-full h-1/6 flex justify-center mt-6">
         {/* name and job title */}
 
         <div className=" w-3/4 h-full    text-white">
-          <div className="bg-red-600    flex-row">
+          <div className="bg-red-600 py-4  bg-opacity-60 flex-row">
             <div className="w-full ">
               <h2 className=" h-fit self-center text-2xl font-mono  ">
                 {" "}
@@ -25,22 +25,20 @@ const About = () => {
 
           {/* about myself section  */}
 
-          <div className="w-full  flex justify-center mt-6">
-            <div className="   bg-gray-600 text-white p-6 ">
+          <div className="w-full   mt-6">
+            <div className="   bg-gray-600 text-white p-6 w-full  ">
               <p className="">
-                
-                My name is Joshua Eghelshi i am a Software Engineer. 
-                I enjoy coding and like to get into the details in making the consumer experiance feel connected to the Clients purpose. 
-                
+                My name is Joshua Eghelshi i am a Software Engineer. I enjoy
+                coding and like to get into the details in making the consumer
+                experiance feel connected to the Clients purpose.
               </p>
-
             </div>
             <div>
-              <button className="p-2 w-30 bg-indigo-600 hover:">Contact Me</button>
+              <button className="p-2 m-4 w-30 bg-red-600 hover:bg-white hover:text-black rounded-xl px-4">
+                Contact Me
+              </button>
             </div>
           </div>
-        
-        
         </div>
       </div>
     </div>
