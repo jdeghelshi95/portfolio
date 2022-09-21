fbf08ff4c024315bbc2f85460ef1e2b269a294b7
Joshua Eghelshi <odin@Odin.localdomain>
2022-09-20 21:25:12 -0400

    edited the portfolio page and added hyperlinks to buttons on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,25 +6,27 @@ const Profile = () => {
     <div className=" bg-gray-400 bg-opacity-50 h-screen">
       <div className=" flex justify-center w-screen h-screen  ">
         <div className=" ">
-             {/*Title Section   */}
-         <div className="w-full h-1/6 flex justify-center mt-6">     
-              
-          
-          <div className=" w-1/2 h-full    text-white">
-            <div className="bg-red-600  flex-row">
-            <div className="w-full ">   
-            <h2 className=" h-fit self-center text-2xl font-mono  "> Software Engineer</h2>
+          {/*Title Section   */}
+          <div className="w-full h-1/6 flex justify-center mt-6">
+            <div className=" w-1/2 h-full    text-white">
+              <div className="bg-red-600  flex-row">
+                <div className="w-full ">
+                  <h2 className=" h-fit self-center text-2xl font-mono  ">
+                    {" "}
+                    Software Engineer
+                  </h2>
+                </div>
+                <div className="w-full">
+                  <h1 className=" h-fit self-center text-2xl font-mono  ">
+                    {" "}
+                    Joshua Eghelshi
+                  </h1>
+                </div>
+              </div>
             </div>
-            <div className="w-full">
-            <h1 className=" h-fit self-center text-2xl font-mono  "> Joshua Eghelshi</h1>
-            </div>
-            </div>
-
           </div>
-        </div>
-      
-      
-      {/* about myself section  */}
+
+          {/* about myself section  */}
           <div className="w-full  flex justify-center mt-6">
             <div className=" w-1/2  bg-gray-600 text-white p-6 ">
               <p className="">
@@ -34,28 +36,40 @@ const Profile = () => {
               </p>
             </div>
           </div>
-       
-        {/* button section leading to projects */}
-       <div className="w-full flex justify-between mt-12">
 
-              <div className="hover:bg-red-400 w-fit text-white bg-black ml-5 rounded-lg p-2">
-                <button><a href=""> Tic-Tac-Toe-Ai </a></button>
-              </div>
-              
-              
-              <div className="hover:bg-red-400 bg-black w-fit text-white rounded-lg p-2">
-                <button className=""><a href="https://videoembeds.herokuapp.com/">VideoHUB</a></button>
-              </div>
-              
-              <div className="hover:bg-red-400 bg-black w-fit text-white mr-5 rounded-lg p-2">
-                <button >Trip-Commander</button>
-              </div>
+          {/* button section leading to projects */}
+          <div className="w-full flex justify-between mt-12">
+            <div className="hover:bg-red-400 w-fit text-white bg-black ml-5 rounded-lg p-2 px-4">
+              <button className="">
+                <a href="https://jdeghelshi95.github.io/Tic-Tac-Toe-Ai/">
+                  Tic-Tac-Toe-AI
+                </a>
+              </button>
+            </div>
 
-       </div>
-            
-       
-       
-        </div>    
+            <div className="hover:bg-red-400 bg-black w-fit text-white rounded-lg p-2 px-4">
+              <button className="">
+                <a href="https://videoembeds.herokuapp.com/">VideoHUB</a>
+              </button>
+            </div>
+
+            <div className="hover:bg-red-400 bg-black w-fit text-white mr-5 rounded-lg p-2 px-4">
+              <button className="">
+                <a href="https://trip-commander.herokuapp.com/">
+                  Trip-Commander
+                </a>
+              </button>
+            </div>
+
+            <div className="hover:bg-red-400 bg-black w-fit text-white mr-5 rounded-lg p-2 px-8">
+              <button className="">
+                <a href="https://resy-front-jgmj2.ondigitalocean.app/">
+                  Resy
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
